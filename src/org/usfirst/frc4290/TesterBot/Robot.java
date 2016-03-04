a19ee8f9b06fa3f684e38a800db551541ea6d63a
Brandon Carlson <Brandon Carlson>
2016-03-04 18:10:03 -0500

    Replaced Jaguar with Talon

--- src/org/usfirst/frc4290/TesterBot/Robot.java
+++ src/org/usfirst/frc4290/TesterBot/Robot.java
@@ -13,7 +13,6 @@ package org.usfirst.frc4290.TesterBot;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
