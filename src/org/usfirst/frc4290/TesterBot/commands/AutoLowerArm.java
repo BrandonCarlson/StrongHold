f9dfec503b9ad53cfd48e6e2134bc52652843cb2
Brandon Carlson <Brandon Carlson>
2016-03-12 10:32:25 -0500

    Auto and arm updates

--- src/org/usfirst/frc4290/TesterBot/commands/AutoLowerArm.java
+++ src/org/usfirst/frc4290/TesterBot/commands/AutoLowerArm.java
@@ -44,7 +44,7 @@ public class AutoLowerArm extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.pneumatics.lowerArm();    	
+    	Robot.pneumatics.raiseArm();    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc4290/TesterBot/commands/AutoRaiseArm.java
+++ src/org/usfirst/frc4290/TesterBot/commands/AutoRaiseArm.java
@@ -44,7 +44,7 @@ public class AutoRaiseArm extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.pneumatics.raiseArm();    	
+    	Robot.pneumatics.lowerArm();    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
